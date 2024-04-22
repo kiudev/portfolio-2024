2d8d2f79acd2907e6cd2639baab965c931b449ff
kiudev <dani.97.saavedra@gmail.com>
2024-04-22 19:39:42 +0200

    rename file

--- src/containers/footer-section/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function FooterSection() {
-   return (
-      <div className="hidden lg:block absolute bottom-0 left-0 text-center text-sm uppercase tracking-widest">
-         <p>Daniel Saavedra ©️2024</p>
-      </div>
-   );
-}
