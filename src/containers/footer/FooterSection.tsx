921dd63cb75e61f254f8b10a6c00e719e59e0af3
kiudev <dani.97.saavedra@gmail.com>
2024-04-22 19:38:58 +0200

    decrease text px

---
+++ src/containers/footer/FooterSection.tsx
@@ -0,0 +1,7 @@
+export default function FooterSection() {
+   return (
+      <div className="hidden lg:block absolute bottom-0 left-0 text-center text-[12px] uppercase tracking-widest">
+         <p>Daniel Saavedra ©️2024</p>
+      </div>
+   );
+}
