7d87d3fdc9d39e359dcef4b7ddd4f5090c435b72
kiudev <dani.97.saavedra@gmail.com>
2024-09-13 17:57:30 +0200

    update ui projects section

--- src/containers/projects/ProjectsSection.tsx
+++ src/containers/projects/ProjectsSection.tsx
@@ -1,50 +1,74 @@
-import Image from 'next/image';
+import Image from "next/image";
+import { useState, RefObject } from "react";
 
 type dataProjects = {
-   name: string;
-   homepage: string;
-   description: string;
-   image: string;
+  name: string;
+  homepage: string;
+  description: string;
+  image: string;
+  isHovered: string | null;
+  onEnter: (name: string) => void;
+  onLeave: () => void;
 };
 
-
-export default function ProjectsSection({ image, name, homepage, description }: dataProjects) {
-   return (
-      <a href={homepage} target="_blank">
-      <div className="bg-blue-400 bg-opacity-20 mt-10 lg:py-1 lg:hover:bg-blue-200 lg:transition-all lg:hover:bg-opacity-20 rounded-md lg:border-t lg:border-opacity-10 lg:border-white-500 border-blue-500 border px-10">
-         <header className="flex flex-row items-center justify-between">
-            <p className="mt-5">{name}</p>
-            <svg
-               xmlnsXlink="http://www.w3.org/1999/xlink"
-               xmlns="http://www.w3.org/2000/svg"
-               viewBox="0 0 20 20"
-               fill="currentColor"
-               className="mt-5"
-               aria-hidden="true"
-               width="20"
-               height="20"
-            >
-               <path
-                  fillRule="evenodd"
-                  d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
-                  clipRule="evenodd"
-                  fill="#E2E8F0"
-               ></path>
-            </svg>
-         </header>
-
-         <p className="text-blue-100 text-opacity-65 mt-3 text-sm">
-            {description}
-         </p>
-
-         <Image
-            className="mt-5 mb-7 border border-white border-blue-100 rounded-md"
+export default function ProjectsSection({
+  image,
+  name,
+  homepage,
+  description,
+  isHovered,
+  onEnter,
+  onLeave,
+}: dataProjects) {
+  return (
+    <a
+      href={homepage}
+      target="_blank"
+      className={`transition-opacity duration-300 ${
+        isHovered === name
+          ? "opacity-100"
+          : isHovered === null
+          ? "opacity-100"
+          : "opacity-50"
+      }`}
+      onMouseEnter={() => onEnter(name)}
+      onMouseLeave={onLeave}
+    >
+      <div className="hover:bg-blue-100 hover:bg-opacity-10 lg:px-6 transition-all rounded-xl">
+        <header className="flex flex-row items-center justify-between">
+          <Image
+            className="mt-5 mb-7 rounded-md aspect-[4/3] object-cover"
             src={image}
             alt="movie"
-            width={200}
-            height={200}
-         />
+            width={100}
+            height={100}
+          />
+          <div className="flex flex-col gap-2 w-full ml-8">
+            <div className="flex flex-row justify-between items-center">
+            <p className="text-blue-100 text-md">{name}</p>
+            <svg
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+              width="20"
+              height="20"
+            >
+              <path
+                fillRule="evenodd"
+                d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                clipRule="evenodd"
+                fill="#E2E8F0"
+              ></path>
+            </svg>
+            </div>
+            <p className="text-blue-100 text-opacity-65 mt-3 text-sm">
+              {description}
+            </p>
+          </div>
+        </header>
       </div>
-   </a>
-   );
+    </a>
+  );
 }
