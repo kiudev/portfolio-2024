edf4c79ad69b2e43e0987425c74e82f2172224d0
kiudev <dani.97.saavedra@gmail.com>
2024-09-13 17:56:43 +0200

    hide container

--- src/containers/footer/FooterSection.tsx
+++ src/containers/footer/FooterSection.tsx
@@ -1,6 +1,6 @@
 export default function FooterSection() {
    return (
-      <div className="hidden lg:block absolute bottom-0 left-0 text-center text-[12px] uppercase tracking-widest">
+      <div className="hidden lg:block  text-[12px] uppercase tracking-widest">
          <p>Daniel Saavedra ©️2024</p>
       </div>
    );
