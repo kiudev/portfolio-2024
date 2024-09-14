8ade77713aa5f5b7ead1e1ef429d2d6dd6eb4b27
kiudev <dani.97.saavedra@gmail.com>
2024-09-14 12:59:12 +0200

    export projects data

---
+++ src/data/projectsItems.js
@@ -0,0 +1,32 @@
+export const projectsItems = [
+  {
+    name: "Cinopolis",
+    image: "/cinopolis.png",
+    homepage: "https://cinopolis.vercel.app",
+    description: "A platform guide for those looking for a movie or tv show",
+  },
+  {
+    name: "EN-ES Translate",
+    image: "/en-es-translate.png",
+    homepage: "https://en-es-translate.vercel.app",
+    description: "Translate between these two languages",
+  },
+  {
+    name: "iOS Calculator",
+    image: "/ios-calculator.png",
+    homepage: "https://calculator-kiudev.vercel.app",
+    description: "iOS Calculator with HTML, CSS and JS",
+  },
+  {
+    name: "Button Customizer",
+    image: "/button-customizer.png",
+    homepage: "https://customize-button.vercel.app",
+    description: "Customize your own button",
+  },
+  {
+    name: "Weather App",
+    image: "/weather-app.png",
+    homepage: "https://any-clime.vercel.app",
+    description: "Weather App",
+  },
+];
