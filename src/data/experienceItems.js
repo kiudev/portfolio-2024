4aec31ffeeccd2f8fe7891fee9065d3c8a898a5a
kiudev <dani.97.saavedra@gmail.com>
2024-09-14 12:58:57 +0200

    export experience data

---
+++ src/data/experienceItems.js
@@ -0,0 +1,11 @@
+export const experienceItems = [
+  {
+    date: "2024 - Present",
+    label: "Internship",
+    title: "Web Developer",
+    company: "StellaTech",
+    content:
+      "Design and optimisation of websites and implementation of SEO strategies to improve search engine visibility.",
+    website: "https://growthweb.es",
+  },
+];
