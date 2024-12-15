1f87182cd7e1a15dd190e7cc0abbfe692ebb0118
kiudev <dani.97.saavedra@gmail.com>
2024-12-15 09:39:13 +0100

    remove useless projects

--- src/data/projectsItems.js
+++ src/data/projectsItems.js
@@ -3,30 +3,6 @@ export const projectsItems = [
     name: "Cinopolis",
     image: "/cinopolis.png",
     homepage: "https://cinopolis.vercel.app",
-    description: "A platform guide for those looking for a movie or tv show",
-  },
-  {
-    name: "EN-ES Translate",
-    image: "/en-es-translate.png",
-    homepage: "https://en-es-translate.vercel.app",
-    description: "Translate between these two languages",
-  },
-  {
-    name: "iOS Calculator",
-    image: "/ios-calculator.png",
-    homepage: "https://calculator-kiudev.vercel.app",
-    description: "iOS Calculator with HTML, CSS and JS",
-  },
-  {
-    name: "Button Customizer",
-    image: "/button-customizer.png",
-    homepage: "https://customize-button.vercel.app",
-    description: "Customize your own button",
-  },
-  {
-    name: "Weather App",
-    image: "/weather-app.png",
-    homepage: "https://any-clime.vercel.app",
-    description: "Weather App",
+    description: "A platform guide to help you find your favourite movies",
   },
 ];
