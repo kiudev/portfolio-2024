df5a81d7e8f3f1ffbfbaece0ee8399ab68d18a8e
kiudev <dani.97.saavedra@gmail.com>
2024-12-18 17:56:33 +0100

    undo projects

--- src/data/projectsItems.js
+++ src/data/projectsItems.js
@@ -5,4 +5,28 @@ export const projectsItems = [
     homepage: "https://cinopolis.vercel.app",
     description: "A platform guide to help you find your favourite movies.",
   },
+  {
+    name: "EN-ES Translate",
+    image: "/en-es-translate.png",
+    homepage: "https://en-es-translate.vercel.app",
+    description: "Translate between these two languages",
+  },
+  {
+    name: "iOS Calculator",
+    image: "/ios-calculator.png",
+    homepage: "https://calculator-kiudev.vercel.app",
+    description: "iOS Calculator with HTML, CSS and JS",
+  },
+  {
+    name: "Button Customizer",
+    image: "/button-customizer.png",
+    homepage: "https://customize-button.vercel.app",
+    description: "Customize your own button",
+  },
+  {
+    name: "Weather App",
+    image: "/weather-app.png",
+    homepage: "https://any-clime.vercel.app",
+    description: "Weather App",
+  },
 ];
